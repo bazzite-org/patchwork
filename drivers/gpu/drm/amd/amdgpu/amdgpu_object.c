torvalds/linux

commit 216c1282dde38ca87ebdf1ccacee5a0682901574

drm/amdgpu: use GTT only as fallback for VRAM|GTT
Try to fill up VRAM as well by setting the busy flag on GTT allocations.

This fixes the issue that when VRAM was evacuated for suspend it's never
filled up again unless the application is restarted.

Signed-off-by: Christian König <christian.koenig@amd.com>
Reviewed-by: Zack Rusin <zack.rusin@broadcom.com>
Link: https://patchwork.freedesktop.org/patch/msgid/20240229134003.3688-2-christian.koenig@amd.com

drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
@@ -173,6 +173,12 @@ void amdgpu_bo_placement_from_domain(struct amdgpu_bo *abo, u32 domain)
 			abo->flags & AMDGPU_GEM_CREATE_PREEMPTIBLE ?
 			AMDGPU_PL_PREEMPT : TTM_PL_TT;
 		places[c].flags = 0;
+		/*
+		 * When GTT is just an alternative to VRAM make sure that we
+		 * only use it as fallback and still try to fill up VRAM first.
+		 */
+		if (domain & abo->preferred_domains & AMDGPU_GEM_DOMAIN_VRAM)
+			places[c].flags |= TTM_PL_FLAG_FALLBACK;
 		c++;
 	}
 

bazzite-org/patchwork

commit f683e78eec449d9106f8c01a7ddfc34c6e6a2a18

drm/amdgpu: apply GTT only as fallback for VRAM|GTT only to dGPUs
Fixes issue caused by 216c1282dde38ca87ebdf1ccacee5a0682901574.

Which causes a crash when AutoUMA is used, see:
https://gitlab.freedesktop.org/drm/amd/-/issues/3704

drivers/gpu/drm/amd/amdgpu/amdgpu_object.c
@@ -179,8 +179,12 @@ void amdgpu_bo_placement_from_domain(struct amdgpu_bo *abo, u32 domain)
 		/*
 		 * When GTT is just an alternative to VRAM make sure that we
 		 * only use it as fallback and still try to fill up VRAM first.
+		 * 
+		 * APUs really hate doing this and will crash when AutoUMA is 
+		 * on, only apply to dGPU cards that actually benefit.
 		 */
-		if (domain & abo->preferred_domains & AMDGPU_GEM_DOMAIN_VRAM)
+		if (!adev->gmc.xgmi.connected_to_cpu && !adev->gmc.is_app_apu &&
+		    domain & abo->preferred_domains & AMDGPU_GEM_DOMAIN_VRAM)
 			places[c].flags |= TTM_PL_FLAG_FALLBACK;
 		c++;
 	}