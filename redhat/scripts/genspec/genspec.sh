164b1f3c6abf9d6fbbb153294f29f6962d9ae0e0
Antheas Kapenekakis <lkml@antheas.dev>
2025-10-03 18:38:43 +0200

    CI: add ZFS module
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>

--- redhat/scripts/genspec/genspec.sh
+++ redhat/scripts/genspec/genspec.sh
@@ -70,6 +70,7 @@ test -f "$SOURCES/$SPECFILE" &&
 	s/%%NVIDIAVERSION%%/$NVIDIAVERSION/
 	s/%%NVIDIAVERSIONREL%%/$NVIDIAVERSIONREL/
 	s/%%NVIDIAVERSIONLTS%%/$NVIDIAVERSIONLTS/
+	s/%%ZFSVERSION%%/$ZFSVERSION/
 	s/%%SPECSELFTESTS_MUST_BUILD%%/$SPECSELFTESTS_MUST_BUILD/" "$SOURCES/$SPECFILE"
 test -n "$RHSELFTESTDATA" && test -f "$SOURCES/$SPECFILE" && sed -i -e "
 	/%%SPECCHANGELOG%%/r $SOURCES/$SPECCHANGELOG
@@ -142,8 +143,8 @@ for dir in $(find $ "$TOPDIR/drivers/custom" -mindepth 1 -maxdepth 1 -type d 2>/
         elif [[ "$modname" == "cdemu" ]]; then
                 SUBDIR="vhba-module/"
 		EXTRA_FILES+=("vhba-module/debian/vhba-dkms.udev")
-	elif [[ ${modname,,} == nvidia* ]]; then
-		# Skip NVIDIA modules
+	elif [[ ${modname,,} == nvidia* || "$modname" == "zfs" ]]; then
+		# Skip NVIDIA/ZFS modules
 		continue
         fi
 
